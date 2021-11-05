95a515b520c3509c33655d5d3de12f9cc972a787
JB Aubrée <jb.aubree@digitsole.com>
2021-11-05 11:23:11 +0100

    update declaration components

--- src/types/main.d.ts
+++ src/types/main.d.ts
@@ -1,22 +1,22 @@
 declare module 'vue' {
   export interface GlobalComponents {
-    MwBadge: typeof import('./components/badge/MwBadge.vue')['default']
-    MwButton: typeof import('./components/button/MwButton.vue')['default']
-    MwButtonGroup: typeof import('./components/button-group/MwButtonGroup.vue')['default']
-    MwButtonLoader: typeof import('./components/button-loader/MwButtonLoader.vue')['default']
-    MwCard: typeof import('./components/card/MwCard.vue')['default']
-    MwDatepicker: typeof import('./components/datepicker/MwDatepicker.vue')['default']
-    MwDropdown: typeof import('./components/dropdown/MwDropdown.vue')['default']
-    MwHeading: typeof import('./components/heading/MwHeading.vue')['default']
-    MwInput: typeof import('./components/input/MwInput.vue')['default']
-    MwList: typeof import('./components/list/MwList.vue')['default']
-    MwListItem: typeof import('./components/list-item/MwListItem.vue')['default']
-    MwModal: typeof import('./components/modal/MwModal.vue')['default']
-    MwP: typeof import('./components/p/MwP.vue')['default']
-    MwPagination: typeof import('./components/pagination/MwPagination.vue')['default']
-    MwSelect: typeof import('./components/select/MwSelect.vue')['default']
-    MwSmall: typeof import('./components/small/MwSmall.vue')['default']
-    MwTable: typeof import('./components/table/MwTable.vue')['default']
+    MwBadge: typeof import('vue-mateilwind/src/components/badge/MwBadge.vue')['default']
+    MwButton: typeof import('vue-mateilwind/src/components/button/MwButton.vue')['default']
+    MwButtonGroup: typeof import('vue-mateilwind/src/components/button-group/MwButtonGroup.vue')['default']
+    MwButtonLoader: typeof import('vue-mateilwind/src/components/button-loader/MwButtonLoader.vue')['default']
+    MwCard: typeof import('vue-mateilwind/src/components/card/MwCard.vue')['default']
+    MwDatepicker: typeof import('vue-mateilwind/src/components/datepicker/MwDatepicker.vue')['default']
+    MwDropdown: typeof import('vue-mateilwind/src/components/dropdown/MwDropdown.vue')['default']
+    MwHeading: typeof import('vue-mateilwind/src/components/heading/MwHeading.vue')['default']
+    MwInput: typeof import('vue-mateilwind/src/components/input/MwInput.vue')['default']
+    MwList: typeof import('vue-mateilwind/src/components/list/MwList.vue')['default']
+    MwListItem: typeof import('vue-mateilwind/src/components/list-item/MwListItem.vue')['default']
+    MwModal: typeof import('vue-mateilwind/src/components/modal/MwModal.vue')['default']
+    MwP: typeof import('vue-mateilwind/src/components/p/MwP.vue')['default']
+    MwPagination: typeof import('vue-mateilwind/src/components/pagination/MwPagination.vue')['default']
+    MwSelect: typeof import('vue-mateilwind/src/components/select/MwSelect.vue')['default']
+    MwSmall: typeof import('vue-mateilwind/src/components/small/MwSmall.vue')['default']
+    MwTable: typeof import('vue-mateilwind/src/components/table/MwTable.vue')['default']
   }
 }
 
@@ -25,55 +25,55 @@ export { }
 declare module 'vue-mateilwind' {
   const install: PluginFunction<{}>
 
-  export declare namespace MwBadge {
+  export namespace MwBadge {
     const install: PluginFunction<{}>
   }
-  export declare namespace MwButton {
+  export namespace MwButton {
     const install: PluginFunction<{}>
   }
-  export declare namespace MwButtonGroup {
+  export namespace MwButtonGroup {
     const install: PluginFunction<{}>
   }
-  export declare namespace MwButtonLoader {
+  export namespace MwButtonLoader {
     const install: PluginFunction<{}>
   }
-  export declare namespace MwCard {
+  export namespace MwCard {
     const install: PluginFunction<{}>
   }
-  export declare namespace MwDatepicker {
+  export namespace MwDatepicker {
     const install: PluginFunction<{}>
   }
-  export declare namespace MwDropdown {
+  export namespace MwDropdown {
     const install: PluginFunction<{}>
   }
-  export declare namespace MwHeading {
+  export namespace MwHeading {
     const install: PluginFunction<{}>
   }
-  export declare namespace MwInput {
+  export namespace MwInput {
     const install: PluginFunction<{}>
   }
-  export declare namespace MwList {
+  export namespace MwList {
     const install: PluginFunction<{}>
   }
-  export declare namespace MwListItem {
+  export namespace MwListItem {
     const install: PluginFunction<{}>
   }
-  export declare namespace MwModal {
+  export namespace MwModal {
     const install: PluginFunction<{}>
   }
-  export declare namespace MwP {
+  export namespace MwP {
     const install: PluginFunction<{}>
   }
-  export declare namespace MwPagination {
+  export namespace MwPagination {
     const install: PluginFunction<{}>
   }
-  export declare namespace MwSelect {
+  export namespace MwSelect {
     const install: PluginFunction<{}>
   }
-  export declare namespace MwSmall {
+  export namespace MwSmall {
     const install: PluginFunction<{}>
   }
-  export declare namespace MwTable {
+  export namespace MwTable {
     const install: PluginFunction<{}>
   }
 }

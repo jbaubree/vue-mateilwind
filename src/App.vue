0265c540c0a1b2c616ed1969827cc54ed5cabe67
JB Aubrée <jb.aubree@digitsole.com>
2021-11-19 16:29:37 +0100

    use teleport for modals

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,50 @@
+<script setup lang="ts">
+const {
+  isRevealed,
+  reveal,
+  confirm,
+  cancel,
+} = useConfirmDialog()
+
+const modalBody = `I always felt like I could do anything. That’s the main
+      thing people are controlled by! Thoughts- their perception
+      of themselves! They're slowed down by their perception of
+      themselves. If you're taught you can’t do anything, you
+      won’t do anything. I was taught I could do everything.`
+
+</script>
 <template>
   Coucou
-  <MwDatepicker label="date" />
   <MwAlert :is-clearable="true">
     Test
   </MwAlert>
   {{ $t('mw.home.welcome') }}
   {{ $t('test') }}
+  <MwDatepicker label="date" />
+
+  <MwButton variant="primary" @click="reveal">
+    Modal
+  </MwButton>
+  <MwModal
+    v-model="isRevealed"
+    size="md"
+    close-on-click-outside
+    has-close-button
+    @cancel="cancel"
+  >
+    <template #title>
+      Modal Title
+    </template>
+    <template #body>
+      {{ modalBody }}
+    </template>
+    <template #footer>
+      <MwButton is-outlined variant="secondary" @click="cancel">
+        Close
+      </MwButton>
+      <MwButton variant="primary" @click="confirm">
+        Save Changes
+      </MwButton>
+    </template>
+  </MwModal>
 </template>
--- src/components/modal/MwModal.vue
+++ src/components/modal/MwModal.vue
@@ -4,7 +4,7 @@ import type { ButtonSize } from '@/types'
 const props = withDefaults(defineProps<{
   modelValue?: boolean
   hasCloseButton?: boolean
-  closeOnClickOutside: boolean
+  closeOnClickOutside?: boolean
   size?: ButtonSize
 }>(), {
   size: 'md',
@@ -22,53 +22,51 @@ if (props.closeOnClickOutside)
 </script>
 
 <template>
-  <transition name="slide-in-up">
-    <div
-      v-if="modelValue"
-      class="flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 justify-center items-center"
-    >
+  <teleport to="body">
+    <transition name="slide-in-up">
       <div
-        ref="rModal"
-        class="relative w-auto mx-auto px-5"
-        :class="{
-          'max-w-sm': size === 'sm',
-          'max-w-2xl': size === 'md',
-          'max-w-5xl': size === 'lg',
-          'max-w-6xl': size === 'xl',
-        }"
+        v-if="modelValue"
+        class="flex overflow-x-hidden overflow-y-auto fixed inset-0 justify-center items-center"
       >
-        <div class="border-0 rounded-lg shadow-md relative flex flex-col w-full bg-white">
-          <div
-            class="flex items-start justify-between p-5 border-b border-solid border-gray-200 rounded-t"
-          >
-            <h3 class="text-3xl font-semibold">
-              <slot name="title" />
-            </h3>
+        <div
+          ref="rModal"
+          class="relative w-auto mx-auto px-5"
+          :class="{
+            'max-w-sm': size === 'sm',
+            'max-w-2xl': size === 'md',
+            'max-w-5xl': size === 'lg',
+            'max-w-6xl': size === 'xl',
+          }"
+        >
+          <div class="border-0 rounded-lg shadow-md relative flex flex-col w-full bg-white">
             <div
-              v-if="hasCloseButton"
-              class="ml-auto w-8 h-8 flex items-center content-center cursor-pointer"
-              @click="emit('cancel')"
+              class="flex items-start justify-between p-5 border-b border-solid border-gray-200 rounded-t"
             >
-              <div class="h-1 w-8 absolute rounded bg-gray-200 transform rotate-45"></div>
-              <div class="h-1 w-8 absolute rounded bg-gray-200 transform -rotate-45"></div>
+              <h3 class="text-3xl font-semibold">
+                <slot name="title" />
+              </h3>
+              <div
+                v-if="hasCloseButton"
+                class="ml-auto w-8 h-8 flex items-center content-center cursor-pointer"
+                @click="emit('cancel')"
+              >
+                <div class="h-1 w-8 absolute rounded bg-gray-200 transform rotate-45"></div>
+                <div class="h-1 w-8 absolute rounded bg-gray-200 transform -rotate-45"></div>
+              </div>
             </div>
-          </div>
-          <div class="relative p-6 flex-auto">
-            <p class="my-4 text-gray-500 leading-relaxed">
-              <slot name="body" />
-            </p>
-          </div>
-          <div
-            class="flex items-center justify-end p-6 border-t border-solid border-gray-200 rounded-b gap-2"
-          >
-            <slot name="footer" />
+            <div class="relative p-6 flex-auto">
+              <p class="my-4 text-gray-500 leading-relaxed">
+                <slot name="body" />
+              </p>
+            </div>
+            <div
+              class="flex items-center justify-end p-6 border-t border-solid border-gray-200 rounded-b gap-2"
+            >
+              <slot name="footer" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </transition>
-  <div
-    v-if="modelValue"
-    class="opacity-25 fixed inset-0 z-40 bg-black transition transition-all duration-300"
-  ></div>
+    </transition>
+  </teleport>
 </template>
